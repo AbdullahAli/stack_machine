baa28440362d9d4e97f18b0162f97b7a7f14ee02
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2014-05-22 13:07:27 +0100

    Simplified processor function by using the processor modules and removed print statments

--- lib/stack_machine.rb
+++ lib/stack_machine.rb
@@ -1,18 +1,19 @@
 require './lib/parser'
 require './lib/stack'
+require './lib/processors'
 
 module StackMachine
   class Machine
     include ::StackMachine::Parser
+    include ::StackMachine::Processors
     attr_accessor :stack
     attr_accessor :input
 
-    OPERATOR_SIGN_TO_OPERATOR_NAME_MAP = {'+' => 'addition', '*' => 'multiplication'} unless defined?(OPERATOR_SIGN_TO_OPERATOR_NAME_MAP)
+    OPERATOR_SIGN_TO_PROCESSOR_MAP = {'+' => 'Addition', '*' => 'Multiplication'} unless defined?(OPERATOR_SIGN_TO_PROCESSOR_MAP)
 
     def stack_machine_emulator(s)
       @input = s
       if valid_input?
-        puts "character\t|  comment\t\t\t\t|  stack\t"
         process
       else
         -1
@@ -23,24 +24,24 @@ module StackMachine
 
     def process
       @stack = ::StackMachine::Stack.new
-      puts "\t\t\t\t\t\t\t|  [empty]" if @stack.empty?
 
-      @input.each_char do |char|
-        print "'#{char}'\t\t|"
-        if is_operator?(char)
-          puts "perform #{OPERATOR_SIGN_TO_OPERATOR_NAME_MAP[char]}\t\t\t|  "
-          @stack.send("process_#{OPERATOR_SIGN_TO_OPERATOR_NAME_MAP[char]}".to_sym)
-        else
-          puts "push #{char} onto the stack\t\t\t|  "
-          @stack.process_integer(char)
-        end
-        puts "\t\t\t\t\t\t\t|  #{@stack.inspect}"
+      @input.each_char do |character|
+        processor_for(character).process(stack, character)
       end
       @stack
     end
 
-    def is_operator?(char)
-      ALLOWED_OPERATORS.include?(char)
+    def processor_for(character)
+      if is_operator?(character)
+        processor = "StackMachine::Processors::#{OPERATOR_SIGN_TO_PROCESSOR_MAP[character]}"
+      else
+        processor = "StackMachine::Processors::Integer"
+      end
+      StackMachine.const_get(processor).new
+    end
+
+    def is_operator?(character)
+      ALLOWED_OPERATORS.include?(character)
     end
   end
 end
